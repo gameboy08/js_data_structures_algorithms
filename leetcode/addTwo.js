c3e96c27914d375c5eb08da30bad07a68cf5eede
Wayne Li <liwayne009@gmail.com>
2019-05-08 15:47:27 +0800

    modify addTwo

--- leetcode/addTwo.js
+++ leetcode/addTwo.js
@@ -5,25 +5,44 @@
  *     this.next = null;
  * }
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- * 要点：
- * 1. 单位数上相加的值 其实就是 sum % 10 余数
- * 2. 进位本身保存在sum中，留给下次循环使用。进位的计算就是 sum = sum / 10
- * 3. sum === 1 的唯一情况就是 结果的最开头需要进一位，也就是整个结果多一位。
- */
 function ListNode(val) {
     this.val = val;
     this.next = null;
 }
+let l1 = new ListNode(2);
+l1_current = l1;
+l1_current.next = new ListNode(4);
+l1_current = l1_current.next;
+l1_current.next = new ListNode(3);
+console.log(l1);
+
+let l2 = new ListNode(5);
+l2_current = l2;
+l2_current.next = new ListNode(6);
+l2_current = l2_current.next;
+l2_current.next = new ListNode(4);
+
+console.log(l2);
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ * Notes：
+ * 1. Each result digit is the modulo "sum % 10"
+ * 2. The "carry" in each result digit in each iteration is kept in "sum" which will be used in the next iteration. 
+ *    The algorithm for calculating "carry" is sum = sum / 10
+ * 3. The only situation when "sum === 1" is when adding the two most significant digits of the inputs results in a carry, 
+ *    which means there will be one more digit (1) on the result digit as the most-significant digit
+ */
+
+//Runtime: 140 ms, faster than 45.28% of JavaScript online submissions for Add Two Numbers.
 var addTwoNumbers = function(l1, l2) {
-    let node = new ListNode(0);
-    let current = node;
-    let p1 = l1;
+    let dummyHead = new ListNode(0); //store as the result; start with an unwanted 0 as initial head.
+    let current = dummyHead;
+    let p1 = l1; 
     let p2 = l2; 
-    let sum = 0;
+    let sum = 0; //to save the sum of two digits, and then save the carry to itself.
     while (p1 !== null || p2 !== null ) {
         if(p1 !== null) {
             sum += p1.val; //1
@@ -34,15 +53,49 @@ var addTwoNumbers = function(l1, l2) {
             p2 = p2.next;
         }
         current.next = new ListNode(sum%10);
-        //这里很巧妙, 不论有没有进位(1或0), 在下一次循环中, 进位就已经加上了, 加在上面的1 或 2处。
-        //这里要用Math.floor, 不然结果可能会保留一位小数。
+        //calculating the carry that will be used in the next turn.
+        //Math.floor is needed, otherwise the "sum" may reserve a decimal.
         sum = Math.floor( sum / 10 );
         current = current.next; //将结果换到下一位。
     }
-    //如果p1, p2的最后一位都加完了，还需要进一位, 就需要在结果上多拓展一位
+    //After exhausting the iteration, if we still got a carry, extend the result one more digit as the most-significant digit
     if (sum === 1) {
         current.next = new ListNode(1);
     }
-    //返回值丢弃了第一个0值。
-    return node.next;
+    //because we start with an unwanted 0, so return from the "next".
+    return dummyHead.next;
 };
+
+console.log(addTwoNumbers(l1, l2));
+
+//Runtime: 112 ms, faster than 100.00% of JavaScript online submissions for Add Two Numbers.
+var addTwoNumbers2 = function (l1, l2) {
+    const dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let p1 = l1, p2 = l2;
+    let sum = 0, carry = 0;
+    while (p1 !== null || p2 !== null || sum > 0) {
+        if (p1 !== null) {
+            sum += p1.val;
+            p1 = p1.next;
+        }
+        if (p2 !== null) {
+            sum += p2.val;
+            p2 = p2.next;
+        }
+
+        if (sum >= 10) {
+            sum -= 10;
+            carry = 1;
+        }
+
+        current.next = new ListNode(sum);
+        current = current.next;
+        sum = carry;
+        carry = 0
+        
+    }
+    return dummyHead.next;
+}
+
+console.log(addTwoNumbers2(l1, l2));
