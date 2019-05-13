9e60e90283508d0b2400701f6461ea0182a2e797
Wayne Li <liwayne009@gmail.com>
2019-05-14 00:03:27 +0800

    doublyLinkedList modification

--- LinkedList/doublyLinkedList.js
+++ LinkedList/doublyLinkedList.js
@@ -10,35 +10,44 @@ function DoublyLinkedList() {
     let tail = null;
 
     this.insert = function(position, element) {
+        //check boundary
         if(position >= 0 && position <= length) {
             let node = new Node(element);
             let current = head;
             let previous;
-            let index = 0;
-            if (position === 0) {
-                if (!head) { //if linked list is empty
+            if (position === 0) { //insert at the beginning.
+                if (!head) { // linked list is empty
                     head = node;
                     tail = node;
                 } else {
-                        node.next = current;
-                        current.prev = node;
-                        head = node;
-                    
+                    node.next = current;
+                    current.prev = node;
+                    head = node;
                 }
-            } else if (position === length) { //last element
+            } else if (position === length) { // insert at the end
                 current = tail;
-                node.prev = current;
                 current.next = node;
+                node.prev = current;
                 tail = node;
             } else {
-                while (index++ < position) {
+                let index = 0;
+                // while (index < length) {
+                //     if (index === position) {
+                //         break;
+                //     } else {
+                //         current = current.next;
+                //         index++;
+                //     }
+                // }
+                // previous = current.prev;
+                while(index++ < position) { //stop when current index equals to position
                     previous = current;
-                    current = current.next;
+                    current = current.next
                 }
+                node.next = current;
+                node.prev = previous;
                 previous.next = node;
                 current.prev = node;
-                node.prev = previous;
-                node.next = current;
             }
             length++;
             return true;
@@ -46,4 +55,35 @@ function DoublyLinkedList() {
             return false;
         }
     }
+
+    this.removeAt = function(position) {
+        if (position >= 0 && position < length) {
+            let current = head;
+            let index = 0
+            let previous;
+            if (position === 0) {
+                head = current.next;
+                if(length === 1) {
+                    tail = null
+                } else {
+                    head.prev = null;
+                }
+            } else if (position === length - 1) {
+                current = tail
+                tail = current.prev
+                tail.next = null;
+            } else {
+                while(index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+                current.next.prev = previous;
+            }
+            length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    } 
 }
